bfea77f5e25025543dc4aef2a6daa02f3608cac3
react-portfolio <bdi-2006@mail.ru>
2025-12-15 17:33:11 +0500

    Add Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,16 @@
 
-export default function Header() { 
-  return (<header className="header"><h1>VeraDasha</h1></header>); 
-} 
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>VeraDasha</h1>
+      <nav className="nav">
+        <ul>
+          <li><a href="/">Главная</a></li>
+          <li><a href="/about">О нас</a></li>
+          <li><a href="/services">Услуги</a></li>
+          <li><a href="/contact">Контакты</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
