261701723d939ae5c46d8d4a80f84d46c5719f92
react-portfolio <bdi-2006@mail.ru>
2025-12-15 17:23:44 +0500

    Add Header, Portfolio, About components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header/Header';
+import Portfolio from './components/Portfolio/Portfolio';
+import About from './components/About/About';
+
+import './components/Header/Header.css';
+import './components/Portfolio/Portfolio.css';
+import './components/About/About.css';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Portfolio />
+      <About />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,4 @@
+// src/components/Header/Header.js 
+export default function Header() { 
+  return (<header className="header"><h1>My Portfolio</h1></header>); 
+} 
